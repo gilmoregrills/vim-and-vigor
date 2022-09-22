9d3fad6b1b9e258ce8bfa75d3ecdb42e4bf72022
Robin Lightfoot <robin.yonge@tractable.ai>
2022-09-22 16:21:23 +0100

    remove stuff im not using anymore

--- install-bash-it.sh
+++
@@ -1,9 +0,0 @@
-# install bash-it
-git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
-~/.bash_it/install.sh
-mkdir ~/.bash_it/themes/robin/
-
-# copy themes and profiles
-cp robin.theme.bash ~/.bash_it/themes/robin/
-cp .bash_profile ~/.bash_profile
-cp .vimrc ~/.vimrc
